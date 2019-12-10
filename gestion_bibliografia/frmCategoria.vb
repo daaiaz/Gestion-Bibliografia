c192909ff5aefff8a51009dd230283eeafcdedc5
MariiiCuenca <mariiicuenca66@gmail.com>
2019-12-10 11:51:10 -0300

    MODIFICANDO....

--- gestion_bibliografia/frmCategoria.vb
+++ gestion_bibliografia/frmCategoria.vb
@@ -1,3 +1,28 @@
 ﻿Public Class frmCategoria
 
+    Function DatosValidos() As Boolean
+
+        If txtDescripcion.Text.Trim() = "" Then
+            MessageBox.Show("Debe ingresar el nombre de la categoria", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtDescripcion.Focus()
+            Return False
+        End If
+        Return True
+
+    End Function
+
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+
+
+
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+
+    End Sub
 End Class
